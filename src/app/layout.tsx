a8c2db2663b353357b87c28072853c017c315569
Plyn-Inc <plynlink@gmail.com>
2026-09-18 17:39:05 +0900

    perf: keep font off initial preload queue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ const pretendard = localFont({
   src: "../../node_modules/pretendard/dist/web/variable/woff2/PretendardVariable.woff2",
   variable: "--font-pretendard",
   display: "swap",
+  preload: false,
   weight: "45 920",
 });
 
